a64118cbb19f40c58751f5ab897956e0316e49fc
Sachin K V <79142104+SachinKVenkateshappa@users.noreply.github.com>
2021-12-10 14:07:30 +0530

    10th dec job skills

---
+++ practice 04 Dec/real_life_example.py
@@ -0,0 +1,9 @@
+
+from playsound import playsound
+
+# # lst1=['wolf','sheep','peacock','lion','cat','tiger']
+# # for i in lst1:
+# #
+# a ='wolf'
+# if a == 'wolf':
+playsound('C:\Users\Admin\PycharmProjectsMCS_00_Sachin_Core_Pythonpractice 04 Dec/sets_tuples_arrays/dog.mp3')
