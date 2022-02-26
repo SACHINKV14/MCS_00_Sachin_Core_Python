63a0e86f1de5d551191dd8e3ddb6eda8ae9713c0
Sachin K V <79142104+SachinKVenkateshappa@users.noreply.github.com>
2022-02-26 17:36:02 +0530

    26 Feb 2022

---
+++ practice 04 Dec/harsha_tasks/_26_Feb_2022/_HR_Feb_26_q13.py
@@ -0,0 +1,22 @@
+"""You are given a spreadsheet that contains a list of  athletes and their details (such as age, height, weight and so on). You are required to sort the data based on the th attribute and print the final resulting table. Follow the example given below for better understanding.
+"""
+
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+    print(arr)
+
+    lst=sorted(arr,key=lambda x:x[1],reverse=False)
+    for i in lst:
+        print(i)
